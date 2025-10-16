ef0d1938aa0dcc5a8c70b8073270f99f73ea56b4
Dom <ds2464@njit.edu>
2025-10-16 13:59:00 -0400

    modified app/calculation.py

--- app/calculation.py
+++ app/calculation.py
@@ -38,8 +38,13 @@ class Calculation:
                 Decimal(pow(float(x), 1 / float(y)))
                 if x >= 0 and y != 0
                 else self._raise_invalid_root(x, y)
-            )
+            ),
+            "Modulus": lambda x, y: x % y if y != 0 else self._raise_div_zero(),
+            "IntegerDivision": lambda x, y: Decimal(x // y) if y != 0 else self._raise_div_zero(),
+            "Percentage": lambda x, y: (x / y) * 100 if y != 0 else self._raise_div_zero(),
+            "AbsoluteDifference": lambda x, y: abs(x - y),
         }
+        
         # Retrieve the operation function based on the operation name
         op = operations.get(self.operation)
         if not op:
